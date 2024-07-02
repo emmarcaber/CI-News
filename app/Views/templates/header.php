8c0826b0e3e99b58a6a7df7e04cde8109375d345
Emmar Caber <caberemmar@gmail.com>
2024-07-02 16:04:55 +0800

    feat: fetch all news and display to view

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -2,6 +2,11 @@
 <html>
 <head>
     <title>CodeIgniter Tutorial</title>
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
     <h1><?= esc($title) ?></h1>
